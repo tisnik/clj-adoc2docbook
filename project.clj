95f7c3c8843f8ea69cb13023178d297a4dcec102
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-11 09:11:44 +0200

    Bump up Clojure version to 1.8

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 ;
-;  (C) Copyright 2017  Pavel Tisnovsky
+;  (C) Copyright 2017, 2020  Pavel Tisnovsky
 ;
 ;  All rights reserved. This program and the accompanying materials
 ;  are made available under the terms of the Eclipse Public License v1.0
@@ -15,7 +15,7 @@
     :url "https://github.com/tisnik/clj-adoc2docbook"
     :license {:name "Eclipse Public License"
               :url "http://www.eclipse.org/legal/epl-v10.html"}
-    :dependencies [[org.clojure/clojure "1.7.0"]
+    :dependencies [[org.clojure/clojure "1.8.0"]
                    [org.clojars.tisnik/cogset-common "0.6.0-SNAPSHOT"]]
     :plugins [[test2junit "1.1.0"]
               [codox "0.8.11"]
